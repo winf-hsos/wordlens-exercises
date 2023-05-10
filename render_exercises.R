88b6a58b8de94913b8088c1a4d0bdb525d1771e0
Meseth <n.meseth@hs-osnabrueck.de>
2023-05-10 12:15:35 +0200

    Restructured exercises

--- render_exercises.R
+++ render_exercises.R
@@ -1,3 +1,4 @@
 library(quarto)
 quarto_render("practice_exercises/", output_format = "pdf", quiet = T)
 quarto_render("mandatory_exercises/", output_format = "pdf", quiet = T)
+quarto_render("exercises/", output_format = "pdf", quiet = T)
